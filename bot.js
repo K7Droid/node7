0d5a29afc6a97820be89c08b96a739892cd6ad92
MAX F.C <maxgt734@gmail.com>
2019-10-17 14:41:44 -0600

    Ya responde a menciones CON IMAGEN

--- bot.js
+++ bot.js
@@ -1,38 +1,37 @@
 console.log ('Bot tweeteador empezando...')
 
-var Twit = require('twit');
+var Twit = require('twit'); //npm para usar la api de twitter
 
-var datos = require('./datos');
+var datos = require('./datos'); //datos = las llaves para usar la api
 var T = new Twit(datos);
 
-var stream = T.stream('user');
+var fs = require('fs'); //Paquete para leer archivos
+
+var b64content = fs.readFileSync('images/auron.png', { encoding: 'base64' }) //base64 de la imagen
+
+var stream = T.stream('statuses/filter', { track: ['@K7Bot1'] }); //detectar cuando le tweeteen a esa cuenta
 
 stream.on('tweet',EventoTweet);
 
-function EventoTweet(event){
-	//var fs = require('fs');
-	//var json = JSON.stringify(event,null,2);
-	//fs.writeFile("conttweet.json",json);
-	var tweeteara = event.in_reply_to_screen_name;
-	var text = event.text;
-	var from = event.user.screen_name;
+function EventoTweet(tweet) { 
 
-	console.log(from+' te acaba de tweetear ALV!');
+    var nombre = tweet.user.screen_name; // Quien envió el tweet
 
-	if(tweeteara == 'k7bot1'){
-		var nuevotweet = '@'+from+'Llega tu imagen!';
-		hacerTweet(nuevotweet)
-	}
-}
+	T.post('media/upload', { media_data: b64content }, function (err, data, response) {
 
-hacerTweet();
-setInterval(hacerTweet,1000*60); //hacer la funcion cada minuto
-
-function hacerTweet(txt){
-
-	var num = Math.floor(Math.random()*100)
-
-	T.post('statuses/update', { status: txt }, function(err, data, response) {
-	  console.log('Funciono! '+data)
+	  var mediaIdStr = data.media_id_string
+	  var txtRespuesta = "¡ @" + nombre + ' ' + 'no olvides tomar agua crack!'; //La respuesta a la mencion
+	  var parametros = { media_id: mediaIdStr, alt_text: { text: txtRespuesta } }
+	 
+	  T.post('media/metadata/create', parametros, function (err, data, response) {
+	    if (!err) {
+	      //agregar media al tweet
+	      var params = { status: txtRespuesta, media_ids: [mediaIdStr] }
+	 
+	      T.post('statuses/update', params, function (err, data, response) {
+	        console.log(data)
+	      })
+	    }
+	  })
 	})
-}
+};
--- datos.js
+++ datos.js
@@ -1,9 +1,7 @@
 
 module.exports ={
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  consumer_key:         '2n6unVvAQXJ83EzZHGOe0fyi3',
+  consumer_secret:      'nj54k689iC2Mu5z5dtYManXQWWSW1VOkeZe7M7miwWVSGHrlIE',
+  access_token:         '312401877-LziVKI7ymfGdC1tjV8vhzug4QMG35skwvjtb81HK',
+  access_token_secret:  '1OyoYp8vqC7pRh27gRXapmIxP3UqTgHZPjRqAZGCdemkU'
 }
