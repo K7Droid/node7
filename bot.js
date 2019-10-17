fb633033f624711705578f3a80cc28342c2cbc32
MAX F.C <maxgt734@gmail.com>
2019-10-16 23:30:55 -0600

    Ya responde a quien lo menciona

--- bot.js
+++ bot.js
@@ -1,24 +1,38 @@
-console.log ('probando...')
+console.log ('Bot tweeteador empezando...')
 
 var Twit = require('twit');
 
 var datos = require('./datos');
 var T = new Twit(datos);
 
-//Buscar Tweets
-var parametros = {
-	q: 'Guatemala',
-	count: 2
+var stream = T.stream('user');
+
+stream.on('tweet',EventoTweet);
+
+function EventoTweet(event){
+	//var fs = require('fs');
+	//var json = JSON.stringify(event,null,2);
+	//fs.writeFile("conttweet.json",json);
+	var tweeteara = event.in_reply_to_screen_name;
+	var text = event.text;
+	var from = event.user.screen_name;
+
+	console.log(from+' te acaba de tweetear ALV!');
+
+	if(tweeteara == 'k7bot1'){
+		var nuevotweet = '@'+from+'Llega tu imagen!';
+		hacerTweet(nuevotweet)
+	}
 }
 
 hacerTweet();
 setInterval(hacerTweet,1000*60); //hacer la funcion cada minuto
 
-function hacerTweet(){
+function hacerTweet(txt){
 
 	var num = Math.floor(Math.random()*100)
 
-	T.post('statuses/update', { status: 'Probando 1..2..'+num }, function(err, data, response) {
+	T.post('statuses/update', { status: txt }, function(err, data, response) {
 	  console.log('Funciono! '+data)
 	})
 }
